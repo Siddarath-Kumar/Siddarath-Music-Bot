e9162d2cfb2a6a89e7c4d3925c4e4a72af0c5165
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-04-28 00:08:48 +1200

    Implemented music bot help command (tested and working)
    - Implemented help.js file embed information

--- bot_commands/help.js
+++ bot_commands/help.js
@@ -15,14 +15,22 @@ module.exports=
                 .setColor('#304281')
                 .setTitle('Music Bot Help Commands')
                 .setDescription('The following is the music bot commands!')
+                .setThumbnail('https://cdn.pixabay.com/photo/2017/05/09/10/03/music-2297759_1280.png')
                 .addFields
                 (
-                    {name: 'Rule 1', value: 'Be Nice'}
+                    {name: '/play search', value: 'Plays a song based on the provided keyword from YouTube.'},
+                    {name: '/play song', value: 'Plays a song based on the provided URL from YouTube.'},
+                    {name: '/play playlist', value: 'Plays a song playlist based on the provided playlist link from YouTube.'},
+                    {name: '/queue', value: 'Shows the current queue of songs.'},
+                    {name: '/quit', value: 'The music bot will stop playing music and disconnect.'},
+                    {name: '/pause', value: 'Will pause the currently playing song.'},
+                    {name: '/resume', value: 'Will resume the song playing in the queue.'},
+                    {name: '/shuffle', value: 'Shuffles the queue of music.'},
+                    {name: '/skip', value: 'Skips the current song and plays the next song in the queue.'},
+                    {name: '/skipto', value: 'Skips the current song and goes to the specified track in the queue.'},
+                    {name: '/info', value: 'Displays information about the current song playing.'}
                 )
-
-                .setImage('https://cdn.iconscout.com/icon/free/png-256/music-1404982-1185691.png')
-                .setFooter({text: `Please check the music bot rules`})
-
+                .setImage('https://cdn.pixabay.com/photo/2021/09/11/03/27/music-6614542_1280.jpg')
 
             await interaction.editReply
             ({
